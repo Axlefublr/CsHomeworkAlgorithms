794f525a2afa74e19b8d51e9ed7eae1da5c29277
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-01-06 23:25:09 +0800

    Users are now greeted, with or without ads depending on whether they are premium users or not

--- Greetings/Program.cs
+++ Greetings/Program.cs
@@ -4,6 +4,37 @@ internal class Program
 {
    private static void Main(string[] args)
    {
+      User user1 = new()
+      {
+         Login = "login",
+         Name = "Mark",
+         IsPremium = false
+      };
+      User user2 = new()
+      {
+         Login = "myfavoritecat",
+         Name = "Alex",
+         IsPremium = true
+      };
+      User user3 = new()
+      {
+         Login = "goblinmode",
+         Name = "Aaron",
+         IsPremium = false
+      };
+      List<User> users = new() { user1, user2, user3 };
+
+      Greet(users);
+   }
+
+   static void Greet(List<User> users)
+   {
+      foreach (User user in users) {
+         Console.WriteLine("Omg hewwo " + user.Name + "!");
+         if (!user.IsPremium) {
+            ShowAds();
+         }
+      }
    }
 
    static void ShowAds()
@@ -14,7 +45,8 @@ internal class Program
          "Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу."
       };
 
-      foreach (string ad in ads) {
+      foreach (string ad in ads)
+      {
          Console.WriteLine(ad);
          Thread.Sleep(1000);
       }

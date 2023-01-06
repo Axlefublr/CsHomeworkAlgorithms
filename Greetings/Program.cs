044f1fd84d76f9d03161934f7c3d458686cf149b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-01-06 22:49:46 +0800

    Made ShowAds() smarter for no reason other than habit

--- Greetings/Program.cs
+++ Greetings/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Greetings;
+
+internal class Program
+{
+   private static void Main(string[] args)
+   {
+   }
+
+   static void ShowAds()
+   {
+      string[] ads = {
+         "Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com",
+         "Купите подписку на МыКомбо и слушайте музыку везде и всегда.",
+         "Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу."
+      };
+
+      foreach (string ad in ads) {
+         Console.WriteLine(ad);
+         Thread.Sleep(1000);
+      }
+   }
+}
